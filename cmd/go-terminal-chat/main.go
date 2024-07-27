15ab499c12fb3a276f086edf580ab4f2767b1236
sabir222 <mrsabir4@gmail.com>
2024-07-28 00:56:45 +0100

    adding the server and the client logic

--- cmd/go-terminal-chat/main.go
+++ cmd/go-terminal-chat/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/Sabir222/go-terminal-chat/pkg/client"
+	"github.com/Sabir222/go-terminal-chat/pkg/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	go func() {
+		http.HandleFunc("/", server.Handler)
+		log.Println("Server started on localhost:8080")
+		// ":8080" instead of *addr
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal("ListenAndServe:", err)
+		}
+	}()
+
+	go func() {
+		client.Client()
+	}()
+
+	select {}
+}
